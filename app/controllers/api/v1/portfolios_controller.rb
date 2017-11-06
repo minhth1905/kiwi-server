80d856013859496ecfb37b4322466168c71c9cb0
Tran Huu Minh <minhth1905@gmail.com>
2017-11-06 22:49:57 +0700

    add api

--- app/controllers/api/v1/portfolios_controller.rb
+++ app/controllers/api/v1/portfolios_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::PortfoliosController < ApplicationController
-  before_action :authenticate_request!
+  before_action :authenticate_request!, only: :create
   skip_before_filter :verify_authenticity_token
 
   respond_to :json
 
   def index
     @portfolios = Portfolio.paginate(:page => params[:page],
-      :per_page => Settings.per_page)
+      :per_page => Settings.per_page).order(id: :desc)
   end
 
   def show
